e15068632c459794cc17276f7efbe949946f6c69
Nate Tan <nktan93@gmail.com>
2019-04-30 19:43:03 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,15 +1,15 @@
-INSERT INTO bears (name, grade, gpa, tardies)
+INSERT INTO bears (id, name, age, gender, color, temperament, alive)
     VALUES ("John", 11, 3.4, 3);
-INSERT INTO bears (name, grade, gpa, tardies)
+INSERT INTO bears (id, name, age, gender, color, temperament, alive)
     VALUES ("Kareem", 10, 2.4, 12);
-INSERT INTO bears (name, grade, gpa, tardies)
+INSERT INTO bears (id, name, age, gender, color, temperament, alive)
     VALUES ("Zak", 12, 4.0, 0);
-INSERT INTO bears (name, grade, gpa, tardies)
+INSERT INTO bears (id, name, age, gender, color, temperament, alive)
     VALUES ("Kim", 11, 2.0, 9);
-INSERT INTO bears (name, grade, gpa, tardies)
+INSERT INTO bears (id, name, age, gender, color, temperament, alive)
     VALUES ("Bob", 10, 1.9, 17);
-INSERT INTO bears (name, grade, gpa, tardies)
+INSERT INTO bears (id, name, age, gender, color, temperament, alive)
     VALUES ("Stef", 9, 2.5, 2);
-INSERT INTO bears (name, grade, gpa, tardies)
+INSERT INTO bears (id, name, age, gender, color, temperament, alive)
     VALUES ("George", 9, 3.9, 1);
 bears    VALUES ("Daryl", 9, 3.5, 5);

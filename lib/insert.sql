27d6d26620590212b226fe71c49f8f36593e55b4
Nate Tan <nktan93@gmail.com>
2019-04-30 19:47:29 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,15 +1,14 @@
-INSERT INTO bears (id, name, age, gender, color, temperament, alive)
-    VALUES ("John", 11, 3.4, 3);
-INSERT INTO bears (id, name, age, gender, color, temperament, alive)
-    VALUES ("Kareem", 10, 2.4, 12);
-INSERT INTO bears (id, name, age, gender, color, temperament, alive)
-    VALUES ("Zak", 12, 4.0, 0);
-INSERT INTO bears (id, name, age, gender, color, temperament, alive)
-    VALUES ("Kim", 11, 2.0, 9);
-INSERT INTO bears (id, name, age, gender, color, temperament, alive)
-    VALUES ("Bob", 10, 1.9, 17);
-INSERT INTO bears (id, name, age, gender, color, temperament, alive)
-    VALUES ("Stef", 9, 2.5, 2);
-INSERT INTO bears (id, name, age, gender, color, temperament, alive)
-    VALUES ("George", 9, 3.9, 1);
-bears    VALUES ("Daryl", 9, 3.5, 5);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES ("Mr. Chocolate", 11, male, brown, angry, 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES ("Rowdy", 10, male, brown, angry, 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES ("Tabitha", 12, male, brown, angry, 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES ("Sergeant Brown", 11, male, brown, angry, 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES ("Melissa", 10, male, brown, angry, 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES ("Grinch", 9, male, brown, angry, 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES ("Wendy", 9, male, brown, angry, 1);
